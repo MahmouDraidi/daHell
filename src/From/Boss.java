8c026e9799467d7755ab54aa5c1070c53a1c780b
Max <Max@DESKTOP-EV8CD8S>
2018-01-23 14:18:47 +0200

    can u see this message?

---
+++ src/From/Boss.java
@@ -0,0 +1,14 @@
+package From;
+
+import javax.swing.JFrame;
+
+
+public class Boss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hellllo guuuuys");
+
+	}
+
+}
